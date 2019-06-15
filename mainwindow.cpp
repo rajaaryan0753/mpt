9a394a606e9eb69d4374f4900964311701ad4719
rajaaryan0753 <rajaryangpj@gmail.com>
2019-06-15 01:31:51 -0400

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -20,29 +20,31 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_pushButton_clicked()
 {
-   // QString idnumber = ui -> lineEdit ->text();
-   // QString servername = "originalservename";
-   /* QString dbname ="dbname";
-    QSqlDatabase db =QSqlDatabase:: addDatabase("QODBC");
-   db.setConnectOptions();
-   QString dan =QString("DRIVER=(SQL Native Client);SERVER=%1;DATABASE=%2,Trusted_Connection=Yes;").arg(servername) .arg(dbname);
-  db.setDatabaseName(dan);
-   if(db.open())*/
- //if(idnumber== "123456")
-       //{
-         //Dialog deilalog;
-         //deilalog SetModal(true);
-         //deilalog exec();
-       //}
-       //else
-       //{
-       //    QMessageBox::warning(this,"PRINT","Try Again or contact security administrator");
-     //  }
-/* db.close();
-   }
-   else
-   {
-       qDebug()<<"Error ="<< db.lastError().text();
-} */
+  
+      #include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QMessageBox>
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    QString idnumber = ui -> lineEdit ->text();
+    if(idnumber== "123456"){
+
+        Dielog =new Dialog(this);
+        Dielog->show();
+    }else {
+ QMessageBox::warning(this,"PRINT"," that didn't work pleaseTry Again or contact security administrator");
+}
 
 }
